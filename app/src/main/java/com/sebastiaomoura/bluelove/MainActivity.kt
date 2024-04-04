c7acc1bb7a21ca59d3a124b7e38757566012bb54
Sebastiao Vinicius Araujo de Moura <sebastianbavini@hotmail.com>
2024-04-04 15:48:46 -0300

    lista BlueTooth

--- app/src/main/java/com/sebastiaomoura/bluelove/MainActivity.kt
+++ app/src/main/java/com/sebastiaomoura/bluelove/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.sebastiaomoura.bluelove
 
+import android.bluetooth.BluetoothManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +9,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        lateinit var mBluetoothManager: BluetoothManager
+        fun foo(context: Context){
+            mBluetoothManager = context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        }
+
+
     }
 }
